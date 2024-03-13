f3c6eedd29c6a62c7b270593dce2ab8438cb50f3
Dan O. <danpatrickoneill@gmail.com>
2024-03-13 18:30:36 -0400

    Removed unused import

--- utils/timeUtils.ts
+++ utils/timeUtils.ts
@@ -1,5 +1,3 @@
-import { effect, signal } from "@preact/signals";
-
 export function getStandardizedMonthDayYearKeyFromDate(date: Date) {
   const month = date.getMonth() + 1;
   const monthString = month < 10 ? "0" + month.toString() : month.toString();
