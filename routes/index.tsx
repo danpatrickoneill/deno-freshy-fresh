91cbffa2e25599e8555fefc7774f84d76dd249f0
Dan O. <danpatrickoneill@gmail.com>
2024-03-19 09:05:01 -0400

    Some polish and removal of the defunct

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,20 +2,19 @@ import { Nav } from "../components/Nav.tsx";
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto bg-[#49475B]">
       <Nav />
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
-          src="/logo.svg"
+          src="/logo.png"
           width="128"
           height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="Dan O'Neill Online logo; the initialism DOO imposed on a cornflower blue background"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Welcome to Dan O'Neill Online</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          I hope you have a good time here.
         </p>
       </div>
     </div>
